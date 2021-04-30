f28f1d3fae4122d8327b2bca0d44f289dbe48f65
Jefferson(Zhe Fan) Li <jeffersonli.li@mail.utoronto.ca>
2021-04-30 14:18:43 -0400

    login page style

--- repository-frontend/src/App.js
+++ repository-frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './login/login';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route path="/dashboard">dashboard</Route>
+                    <Route path="/img">img </Route>
+                    <Route path="/">
+                        <Login />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- repository-frontend/src/index.js
+++ repository-frontend/src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { LightTheme, DarkTheme, BaseProvider, styled } from 'baseui';
+import { Provider as StyletronProvider } from 'styletron-react';
+
+const engine = new Styletron();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <StyletronProvider value={engine}>
+            <BaseProvider theme={LightTheme}>
+                <App />
+            </BaseProvider>
+        </StyletronProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
